864d6f90658401841e0477364b09d41c702cbfda
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2024-07-31 12:55:53 +0530

    added env code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
