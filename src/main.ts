d75d72441174ca47cfe4bd24b231e2fb734c6725
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2024-07-31 13:04:04 +0530

    enable prettier configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ConfigModule } from '@nestjs/config';
+
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
