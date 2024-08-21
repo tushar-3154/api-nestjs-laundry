b9b39ada98eca53949ea42bfe62e1650306bf50b
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2024-08-21 14:11:47 +0530

    changes migration to insert initial settings data

--- src/database/migrations/1724229394450-InsertDataForSettings.ts
+++ src/database/migrations/1724229394450-InsertDataForSettings.ts
@@ -1,11 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class InsertDataForSettings1724229394450 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            INSERT INTO settings (setting_key, value) VALUES ('estimate_pickup_normal_hour', '50'),('estimate_pickup_express_hour', '24')
+          `);
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+            DELETE FROM settings WHERE setting_key IN ('estimate_pickup_normal_hour', 'estimate_pickup_express_hour')
+          `);
+  }
 }
